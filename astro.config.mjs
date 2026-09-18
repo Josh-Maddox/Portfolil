d957cfe1f70e1a02685af4e8489752ce4a04f8fd
Josh-Maddox <jjmaddox@student.fullsail.edu>
2026-09-18 11:18:05 -0700

    Pushing Pages for Link
    This is push 1 for link github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-//// @ts-check
-//import { defineConfig } from 'astro/config';
-
-//// https://astro.build/config
-//export default defineConfig({});
-
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+	site: 'https://Josh-Maddox.github.io',
+	base: '/Portfoli',
+
 	vite: {
 		server: {
 			watch: {
